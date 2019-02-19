60b268bd5141b8434f68cc843f1b39f62d1e0596
David S. Jackson <deepbsd@yahoo.com>
2019-02-19 12:43:40 -0500

    updated comments in closures1

--- closures1.js
+++ closures1.js
@@ -1,6 +1,8 @@
 // How could you use this ability
 // in  JS?
 
+// Each closure has a SEPARATE execution context.  It's references will be different 
+// with each call. The number variable will never be the same on any two closures... 
 
 function choosePresident(party){
     // number will get chosen for each separate call
